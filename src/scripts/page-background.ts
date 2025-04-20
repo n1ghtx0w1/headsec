3d64f82d18f7f4a43edfe6c5504154a6f82871a7
n1ghtx0w1 <rhead105@gmail.com>
2025-04-20 13:24:40 -0500

    text

--- src/scripts/page-background.ts
+++ src/scripts/page-background.ts
@@ -256,7 +256,7 @@ class PageBackground {
  * Loads the Geist Mono font. We have to do this asynchronously because the font is not preloaded.
  */
 async function loadFont() {
-  const font = new FontFace('Geist Mono', 'url(/fonts/GeistMono.woff2)');
+  const font = new FontFace('Geist Mono', 'url(/headsec/fonts/GeistMono.woff2)');
 
   await font.load();
   
