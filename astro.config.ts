26d39b994f3eebe237717bdf5c4011f8ec70c892
n1ghtx0w1 <rhead105@gmail.com>
2025-04-20 00:37:50 -0500

    .

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import spectre from './package/src';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 
 // import { spectreDark } from './src/ec-theme';
 
@@ -13,7 +13,7 @@ export default defineConfig({
     enabled: false
   },
   site: 'https://n1ghtx0w1.github.io/headsec/',
-  base: '/headsec/', 
+  base: '/headsec',
   integrations: [
     expressiveCode({
       themes: ['github-dark-default'],
